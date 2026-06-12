5dca4d2570e5d75aec5b76765e5ed693fad0a666
Kiya2595 <Kiya2595@gmail.com>
2026-06-11 21:43:50 -0700

    Day 2: Arithmetic operators completed

---
+++ DAY-2/arithmetic_operators.py
@@ -0,0 +1,18 @@
+#I know this things before but i want to refesh my memory and also i want to start my DSA journey from scratch
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+#Arithmetic operators
+print("SUM: ", a+b)
+print("SUBTRACTION: ", a-b)
+print("MULTIPLICATION: ", a*b)
+print("DIVISION: ", a/b)
+print("REMAINDER: ", a%b)
+print("FLOOR DIVISION: ", a//b) ##we use this when we want to get the quotient without the decimal parrt
+
+##HackerRank Task
+print("-----HackerRank Task-----")
+a= int(input())
+b= int(input())
+print(a+b)
+print(a-b)
+print(a*b)
